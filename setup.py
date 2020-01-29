b87c2009b695e304f1e9183ecc899ac2d4d5b443
dlanier <lanier4@illinois.edu>
2020-01-29 12:29:32 -0600

    package install working

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 """
 # Create dist files:
-pip3 install --upgrade setuptools wheel
-python3 setup.py sdist bdist_wheel
+pip3 install --upgrade setuptools wheel				# install packages needed to build distribution files
+python3 setup.py sdist bdist_wheel					# build package in the dist/ directory
+
+python3 -m pip install --user --upgrade twine		# install package needed to upload the distribution
+
+# username and password required: use __token__ and the API token created in your account
+python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/pychunklbl-x.x.x*
+
+# after successful upload; install the package with pip
+pip3 install -i https://test.pypi.org/simple/ pychunklbl==x.x.x
 """
 from setuptools import setup, find_packages
 from os import path
@@ -12,7 +20,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as fh:
     readme_text = fh.read()
 
 setup(name='pychunklbl',
-	version='0.0.2',
+	version='0.0.5',
 	long_description=readme_text,
 	long_description_content_type='text/markdown',
 	author='Mayo-NCSA DigiPath_MLTK development team',
@@ -24,20 +32,5 @@ setup(name='pychunklbl',
 	'Programming Language :: Python :: 3.8',
 	"Operating System :: OS Independent"],
 	python_requires='>=3.5',
-	package_dir={'': 'pychunklbl'},
-	packages=find_packages(where='pychunklbl'),
+	packages=['pychunklbl'],
 	project_urls={'Source': 'https://github.com/ncsa/DigiPath_MLTK'})
-
-"""
-# Upload to test pypi:
-python3 -m pip install --user --upgrade twine
-
-# with __token__ (the one from your test pypi account)
-python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
-
-# install test package:
-pip3 install -i https://test.pypi.org/simple/ pychunklbl==0.0.1
-
-"""
-
-
