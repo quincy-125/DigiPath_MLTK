d3b9914802bfb0d2a953b6b6cf7e97c7fac0437e
dlanier <lanier4@illinois.edu>
2020-02-05 14:06:40 -0600

    v 0.0.13

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as fh:
     readme_text = fh.read()
 
 setup(name='pychunklbl',
-	version='0.0.12',
+	version='0.0.13',
 	long_description=readme_text,
 	long_description_content_type='text/markdown',
 	author='Mayo-NCSA DigiPath_MLTK development team',
